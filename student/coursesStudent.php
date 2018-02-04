346090df4800810cad595464ad6b40cc041b5937
Marian Isabel <yaneemozo@gmail.com>
2018-02-04 11:28:07 +0800

    Update coursesStudent.php

--- student/coursesStudent.php
+++ student/coursesStudent.php
@@ -1,9 +1,7 @@
 <html>
 <head>
-	<link rel='stylesheet' href="css/mainCSS.css">
-	<link rel='stylesheet' href="css/dashboardStudentCSS.css">
-	<link rel='stylesheet' href="css/accountStudentCSS.css">
 	<link rel='stylesheet' href="css/coursesStudentCSS.css">
+	<link rel='stylesheet' href="css/mainCSS.css">
 	<link rel='stylesheet' href='../bootstrap/css/bootstrap.min.css'>
 	<link rel='stylesheet' href='../datatable_files/datatables.min.css'>
 	<script src="../bootstrap/js/jquery.min.js"></script>
@@ -58,7 +56,7 @@
 			<div class='row'>
 				<!-- SIDE MENU -->
 				<div class='col col-md-2 course_side_menu'>
-					<ul class='nav nav-pills nav-stacked'>
+					<ul class='nav nav-pills nav-stacked course_navSide'>
 						<li role='presentation' class='active'>
 							<a href="coursesStudent.php"> Home </a>
 						</li>
@@ -66,13 +64,10 @@
 							<a href="announcementStudent.php"> Announcements </a>
 						</li>
 						<li role='presentation'>
-							<a href="coursesStudent.php"> Assignment </a>
+							<a href="assignmentStudent.php"> Assignment </a>
 						</li>
 						<li role='presentation'>
 							<a href="courseStudentGrades.php"> Grades </a>
-						</li>
-						<li role='presentation'>
-							<a href="filesStudent.php"> Files </a>
 						</li>
 						<li role='presentation'>
 							<a href="coursesStudent.php"> Syllabus </a>
